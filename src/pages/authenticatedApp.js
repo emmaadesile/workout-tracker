866770a46421ba2e4e513d78fca8ca8d4bab87dc
Emmanuel Adesile <emmanueladesile@Emmanuels-MacBook-Pro.local>
2020-06-10 22:22:30 +0100

    setup app auth

---
+++ src/pages/authenticatedApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AunthenticatedApp(props) {
+  return (
+    <div>
+      <p>Authenticated App</p>
+    </div>
+  );
+}
+
+export default AunthenticatedApp;
---
+++ src/pages/unauthenticatedApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UnAunthenticatedApp(props) {
+  return (
+    <div>
+      <p>Un-Authenticated App</p>
+    </div>
+  );
+}
+
+export default UnAunthenticatedApp;
